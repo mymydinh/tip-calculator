30798feeef6e09718025ae665842a9b11e5f1301
Mymy <thoaimydinh@gmail.com>
2019-06-12 19:35:45 -0700

    fixed some bugs

--- assets/javascript/calculator.js
+++ assets/javascript/calculator.js
@@ -1,25 +1,101 @@
-// function tip15 () {
-//     var billInput = document.getElementById('bill_input').value;
-//     billInput = parseFloat(billInput);
-//     console.log("Bill Input:", billInput);
+function tip15() {
+    var billInput = document.getElementById('bill_input').value;
+    // var billInput = $('#bill_input').val();
+    console.log(`Bill input: ${billInput}`)
 
-//     var billSplit = document.getElementById('split_input')
+    var tipTotal = billInput * .15;
+    // $('#tip_total').val() = tipTotal.toFixed(2);
+    document.getElementById('tip_total').value = tipTotal.toFixed(2);
+    console.log(`Tip total: ${tipTotal}`)
 
-//     var tipTotal = billInput * .15;
-//     document.getElementById('tip_total').value = tipTotal.toFixed(2);
-//     console.log("Tip Total:", tipTotal);
+    var billSplit = document.getElementById('split_input').value;
+    // var billSplit = $('#split_input').val();
 
-//     var billTotal = billInput + tipTotal;
-//     document.getElementById('bill_total').value = billTotal.toFixed(2);
-//     console.log("Bill Total:", billTotal);
-// }
+    var splitTip = (tipTotal) / billSplit;
+    document.getElementById('split_tip').value = splitTip.toFixed(2);
+    // $('#split_tip').val() = splitTip.toFixed(2);
+    console.log(`Split tip: ${splitTip}`)
+
+    var splitTotal = (billInput / billSplit) + splitTip;
+    document.getElementById('split_total').value = splitTotal.toFixed(2);
+    // $('#split_total').val() = splitTotal.toFixed(2);
+    console.log(`Split total: ${splitTotal}`)
+}
+
+function tip18() {
+    var billInput = document.getElementById('bill_input').value;
+    console.log(`Bill input: ${billInput}`)
+
+    var tipTotal = billInput * .18;
+    document.getElementById('tip_total').value = tipTotal.toFixed(2);
+    console.log(`Tip total: ${tipTotal}`)
+
+    var billSplit = document.getElementById('split_input').value;
+
+    var splitTip = (tipTotal) / billSplit;
+    document.getElementById('split_tip').value = splitTip.toFixed(2);
+    console.log(`Split tip: ${splitTip}`)
+
+    var splitTotal = (billInput / billSplit) + splitTip;
+    document.getElementById('split_total').value = splitTotal.toFixed(2);
+    console.log(`Split total: ${splitTotal}`)
+}
 
 function tip20() {
     var billInput = document.getElementById('bill_input').value;
+    console.log(`Bill input: ${billInput}`)
+
     var tipTotal = billInput * .20;
     document.getElementById('tip_total').value = tipTotal.toFixed(2);
+    console.log(`Tip total: ${tipTotal}`)
+
     var billSplit = document.getElementById('split_input').value;
 
-    var billTotal = (tipTotal) / billSplit;
-    document.getElementById('bill_total').value = billTotal.toFixed(2);
+    var splitTip = (tipTotal) / billSplit;
+    document.getElementById('split_tip').value = splitTip.toFixed(2);
+    console.log(`Split tip: ${splitTip}`)
+
+    var splitTotal = (billInput / billSplit) + splitTip;
+    document.getElementById('split_total').value = splitTotal.toFixed(2);
+    console.log(`Split total: ${splitTotal}`)
+}
+
+
+// document.getElementById('other').style.display = 'none';
+
+// function tipOther(id) {
+//     document.getElementById('other').style.display = 'block';
+
+//     var other = document.getElementById('other');
+//     if (other.style.display == 'block') {
+//         other.style.display = 'none';
+//     } else {
+//         other.style.display = 'block';
+//     }
+// }
+
+$('#other').hide();
+
+function tipOther() {
+    $('#other').show();
+
+    var billInput = document.getElementById('bill_input').value;
+    console.log(`Bill input: ${billInput}`)
+
+    otherTip = document.getElementById('other_input').value;
+    console.log(`Other tip: ${otherTip}`)
+
+    var tipTotal = (otherTip / 100) * billInput;
+    document.getElementById('tip_total').value = tipTotal.toFixed(2);
+    console.log(`Tip total: ${tipTotal}`)
+
+    var billSplit = document.getElementById('split_input').value;
+
+    var splitTip = (tipTotal) / billSplit;
+    document.getElementById('split_tip').value = splitTip.toFixed(2);
+    console.log(`Split tip: ${splitTip}`)
+
+    var splitTotal = (billInput / billSplit) + splitTip;
+    document.getElementById('split_total').value = splitTotal.toFixed(2);
+    console.log(`Split total: ${splitTotal}`)
 }
